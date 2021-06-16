61bae90bbc7d555862af52cab0f9a12abd786574
Andrew011297 <32956505+Andrew011297@users.noreply.github.com>
2021-06-16 13:53:47 +0100

    Update database_conn.php

--- PHP_exercises/database_conn.php
+++ PHP_exercises/database_conn.php
@@ -1,5 +1,5 @@
 <?php
-$dbConn = new mysqli('localhost', 'unn_w16011147', 'Fasran1997', 'unn_w16011147');
+$dbConn = new mysqli('localhost', 'unn_w16011147', 'password', 'unn_w16011147');
 
 if ($dbConn->connect_error) {
     echo "<p>Connection failed: ".$dbConn->connect_error."</p>\n";
