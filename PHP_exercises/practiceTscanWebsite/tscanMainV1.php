0a443ec330c2193239b6040b3743006fba24a426
Andrew011297 <32956505+Andrew011297@users.noreply.github.com>
2021-06-16 15:23:37 +0100

    Add files via upload

---
+++ PHP_exercises/practiceTscanWebsite/tscanMainV1.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href ="style.css" rel="stylesheet" type="text/css" />
+    <style>
+      <?php include "style.css" ?>
+    </style>
+    <title>T-Scan Solutions Website</title>
+  </head>
+  <body>
+    <?php
+    echo '<div class="container">
+
+            <div id="logo">
+              <img id="logo" src="/www/Images/tscan_logo.jpg" alt="tscan_logo">
+            </div <br>
+
+            <div id="buttonContainer"> <ul>
+              <li><a href="#Main">Main</a></li>
+              <li><a href="#Remote Support">Remote Support</a></li>
+              <li><a href="#Contact Us">Contact Us</a></li>
+              <li><a href="#About Us">About Us</a></li>
+            </div>
+
+            <div class="textcontainer">
+              <div class="maintext1">
+                <h1> T-Scan Monitoring Solutions </h1>
+
+                <p1> T-Scan provides 24/7 wireless temperature monitoring solutions.<br>
+                We are an innovative, dynamic and flexible business that is constantly developing
+                our system to meet the challenges of the ever changing legislative environment to ensure complete traceability.<br>
+                Our in house development teams primary focus is to provide a multi feature comprehensive system to meet our customers needs and at the same time deliver a reporting platform, that with minimal training, is easy to use.<br>
+                Once installed we support our system with unlimited Remote Assistance which allows our technical team to connect securely to a clients PC and resolve any software questions.<br>
+                t-Scan web is our latest innovative product that ensures T-Scan remain at the forefront of the wireless monitoring markets. </p1>
+              </div>
+
+              <div class="verticalline"></div>
+
+              <div class="maintext2">
+
+                <h1> Lorem ipsum dolor </h1>
+
+                <p2> sit amet, consectetur adipiscing elit. Proin id mattis neque, at placerat erat. Cras at laoreet tortor. Ut vitae nibh placerat, iaculis mi eu, egestas arcu. Nulla facilisi. Maecenas vestibulum elit sed neque fringilla accumsan. Ut eget tincidunt velit. Duis vehicula orci sit amet nunc commodo, mattis malesuada elit venenatis. In hac habitasse platea dictumst. Vestibulum tristique, ipsum nec vestibulum suscipit, magna ligula fermentum elit, quis condimentum felis nunc a felis. Suspendisse eu mauris nec est egestas ultricies sed sit amet nisl. Quisque a magna euismod mauris pellentesque tristique ac non felis. </p2>
+              </div>
+
+              <div class="maintext3">
+
+                <h2> Integer tristique </h2>
+
+                <p3> quis purus eget tincidunt. Vivamus eget auctor nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id ornare sem. Etiam turpis mauris, feugiat vel lectus mollis, imperdiet sodales eros. Etiam sit amet malesuada sapien. Nunc aliquam tellus quam, eu congue diam scelerisque vel. Duis libero augue, consectetur a dolor at, pulvinar malesuada purus. Aliquam gravida varius odio eget faucibus. Aenean finibus consequat nisi sit amet sagittis. Proin imperdiet commodo enim at mattis. Curabitur mollis condimentum sem a consectetur. </p3>
+                </div>
+
+              <div class="newsletter">
+                <h3> Newsletter </h3>
+                <p> Type in your email to apply for our newsletter. </p>
+                <input type="text" id="newsletter"/>
+                <button type="button" onclick="alert("")">Submit</button>
+              </div>
+
+            </div>
+
+            <div id="footerdiv">
+            <footer>
+              <b class="footerbutton" href="#Conditions">Terms & Conditions</b>
+              <b class="footerbutton" href="#Privacy">Privacy Notice</b>
+              <b class="footerbutton" href="#Cookies">Cookies Notice</b>
+              <p> &#169 T-scan Ltd. 2021. </p>
+            </div>
+          </div>
+               ';
+
+              /*<div>
+                  <h2> Remote Support </h2>
+                  <p> Type the code you received from your representative and click the button to connect. </p>
+                  <input type="text" id="code"/>
+                  <button type="button" onclick="alert("")">Connect</button></div></div>*/
+     ?>
+  </body>
+</html>
